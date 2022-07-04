8433f7d75ebe66f9b062c99c968e5fc78752daa6
Samuel Valencia <104396696+SamuelValencia24@users.noreply.github.com>
2022-07-04 11:14:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+Console.WriteLine(IdadeRetorno(idade));
+
+static string IdadeRetorno(int idade)
+{
+    if (idade <= 12)
+    {
+        return "Criança";
+    }
+    else if (idade >= 12 && idade <= 18)
+    {
+        return "Adolescente";
+    }
+    return "Adulto";
+}
